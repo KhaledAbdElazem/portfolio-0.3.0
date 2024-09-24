4ac6de0da6d4859e8095e872054cdde83d9e5f4d
Khaled-AbdElazem <khaledmoustafa400@gmail.com>
2024-09-24 04:46:00 +0300

    NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,17 @@ import './style.css'
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";  // Your Home component
-// import AnotherPage from "./components/AnotherPage";  // Example of another component/page
+import NavBar from "./components/Navbar";  // Example of another component/page
 // import NotFound from "./components/NotFound";  // Optional: for 404 handling
 import "bootstrap/dist/css/bootstrap.min.css"; // Ensure Bootstrap is imported
 
 const App: React.FC = () => {
   return (
     <Router>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />  {/* Home route */}
-        {/* <Route path="/another" element={<AnotherPage />} />  Another page */}
+        {/* <Route path="/src/" element={<AnotherPage />} />  Another page */}
         {/* <Route path="*" element={<NotFound />} />  Catch-all route for 404 */}
       </Routes>
     </Router>
